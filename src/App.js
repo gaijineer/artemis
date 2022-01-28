e87615e364e203c0e75b9f33a2c96a66d632278c
gaijineer <gaijineer@gmail.com>
2022-01-28 20:01:19 +0900

    Quick dirty working ver

--- src/App.js
+++ src/App.js
@@ -1,65 +1,115 @@
+import {
+  getMockElement,
+  mockFib,
+  mockPOComments,
+  mockScrumStatus,
+  mockScrumStoryRoles,
+  mockWords,
+} from "./components/mockData";
 import { useState, useEffect } from "react";
+import { getFromDB, postToDB, putToDB, delFromDB } from "./components/apiReq";
 import StoryAdder from "./components/StoryAdder";
-import Story from "./components/Story";
-import stories from "./data/stories";
+import StoryForm from "./components/StoryForm";
 
 function App() {
-  const [myStories, setMyStories] = useState(stories);
+  const [stories, setStories] = useState([{}]);
   const [newStory, setNewStory] = useState({
-    uid: Date.now(),
-    priority: myStories.length + 1,
-    as: "",
-    want: "",
-    complexity: 3,
-    po: "unseen",
-    status: "future",
+    as: getMockElement(mockScrumStoryRoles).join(),
+    complexity: getMockElement(mockFib)[0],
+    po: getMockElement(mockPOComments).join(""),
+    priority: Math.floor(Math.random() * 10 + 1),
+    status: getMockElement(mockScrumStatus).join(""),
+    want: getMockElement(mockWords, 5).join(" "),
+    dev: getMockElement(mockWords, 100).join(" "),
   });
-  function handleChange(event) {
-    setMyStories((prevState) => {
-      const newState = [...prevState];
-      newState[event.target.parentNode.id - 1][event.target.name] = event.target.value;
-      return newState;
-    });
-  }
 
-  function handleStoryAdderSubmit(event) {
-    event.preventDefault();
-    setMyStories((prevState) => {
-      const newState = [...prevState];
-      newState.splice(parseInt(newStory.priority) - 1, 0, newStory);
-      setNewStory({
-        uid: Date.now(),
-        priority: myStories.length + 1,
-        as: "",
-        want: "",
-        complexity: 3,
-        po: "unseen",
-        status: "future",
-      });
-      return newState;
-    });
-  }
-
-  function handleStoryAdderChange(event) {
+  function handleNewStoryChange(event) {
     setNewStory((prevState) => {
-      return { ...prevState, [event.target.id]: event.target.value };
+      return { ...prevState, [event.target.name]: event.target.value };
     });
   }
 
-  const mappedMyStories = myStories.map((element) => {
-    return <Story key={element.uid} {...element} onChange={handleChange}></Story>;
+  function handleChange(event) {
+    setStories((prevState) => {
+      return prevState.map((element) => {
+        return event.target.parentNode.id != element._id
+          ? element
+          : { ...element, [event.target.name]: event.target.value };
+      });
+    });
+  }
+
+  function handleClick(storyID) {
+    delFromDB(storyID)
+      .then(() => {
+        return getFromDB(stories);
+      })
+      .then((stories) => {
+        setStories(stories);
+      });
+  }
+
+  function handleNewStorySubmit() {
+    postToDB(newStory)
+      .then(() => {
+        setNewStory({
+          as: getMockElement(mockScrumStoryRoles).join(),
+          complexity: getMockElement(mockFib)[0],
+          po: getMockElement(mockPOComments).join(""),
+          priority: Math.floor(Math.random() * 10 + 1),
+          status: getMockElement(mockScrumStatus).join(""),
+          want: getMockElement(mockWords, 5).join(" "),
+        });
+      })
+      .then(() => {
+        return getFromDB(stories);
+      })
+      .then((stories) => {
+        setStories(stories);
+      });
+  }
+
+  function handleSubmit(storyID) {
+    const storyToPut = stories.filter((element) => {
+      return element._id == storyID;
+    });
+    putToDB(storyToPut[0])
+      .then(() => {
+        return getFromDB(stories);
+      })
+      .then((stories) => setStories(stories));
+  }
+
+  // api on component mount
+  useEffect(() => {
+    getFromDB(stories).then((stories) => {
+      setStories(stories);
+    });
+  }, []);
+
+  const mappedStories = stories.map((element, i) => {
+    const bgColorClassNames = ["bg-red-100", "bg-orange-100", "bg-purple-100", "bg-emerald-100"];
+    return (
+      <StoryForm
+        key={element._id}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+        onClick={handleClick}
+        {...element}
+        bgColor={bgColorClassNames[i % 4]}
+      />
+    );
   });
 
   return (
     <main className="w-10/12 p-2 m-auto text-center">
       <StoryAdder
         newStory={newStory}
-        onSubmit={handleStoryAdderSubmit}
-        onChange={handleStoryAdderChange}
+        onChange={handleNewStoryChange}
+        onSubmit={handleNewStorySubmit}
       />
-      {mappedMyStories}
+      {mappedStories}
     </main>
   );
 }
-
 export default App;
